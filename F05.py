cc0ed5fcdebf7738be52f710df5b99f87c57f16f
Maull09 <maulanafirdaus63@gmail.com>
2023-04-08 21:03:12 +0700

    revisi F05

--- F05.py
+++ F05.py
@@ -4,8 +4,8 @@ def ubahjin() :
     username = input("Masukkan username jin : ")
     peubah = False
     ubahrolejin = False
-    i = 1
-    while data.users[i][0] != "stop" :
+    
+    for i in range(100) :
         if data.users[i][0] == username :
             if data.users[i][2] == "pengumpul" :
                 peubah = True
@@ -28,7 +28,7 @@ def ubahjin() :
                     ubahrolejin = False
                     break
             
-        i += 1
+        
 
 
     if peubah == True and ubahrolejin == True :
