eab3a5602115b375a3427908c27473c5a2ad1d2e
Petani Kode <contoh@petanikode.com>
2023-04-11 14:22:27 +0700

    Done

--- F04.py
+++ F04.py
@@ -6,7 +6,7 @@ def hapusjin() :
 
     for i in range(1,1001) :
         if data.users[i][0] == username :
-            confirm = input("Apakah anda yakin ingin menghapus jin dengan username {username} ?")
+            confirm = input(f"Apakah anda yakin ingin menghapus jin dengan username {username} ?")
             if confirm == "Y" or confirm == "y" :
                 for j in range(3) :
                     data.users[i][j] = 0
@@ -15,7 +15,7 @@ def hapusjin() :
         else :
             hapus = False
 
-    if hapus ==False :
-        print("Tidak ada username tsb")
+    if hapus == False :
+        print("Tidak ada jin dengan username tersebut.")
     elif hapus == True :
-        print("data terhapus")
+        print("Jin telah berhasil dihapus dari alam gaib.")
