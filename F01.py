41bedd3fcbd9eabe32573925d73da17f7668d098
Maull09 <maulanafirdaus63@gmail.com>
2023-04-12 16:14:31 +0700

    command dan F14

--- F01.py
+++ F01.py
@@ -4,8 +4,7 @@ def login() :
     username = input("Username : ") #meminta masukkan username
     password = input("Password : ") #meminta masukkan password
     data.usernamee = username
-    u_found = False
-    p_found = False
+    account_found = False
     # iterate over the rows in the users list
     
     #mengecek apakah akun tersebut ada atau tidak, jika tidak, login gagal
@@ -13,19 +12,20 @@ def login() :
         # check if the username and password match
         if username == data.users[i][0]:
             if password == data.users[i][1]:
-                u_found = True  
-                p_found = True
                 data.login_status = "true"
                 data.role = data.users[i][2]
+                account_found = True
+                print(f"Selamat datang, {username}!")
+                print("Masukkan command 'help' untuk daftar command yang dapat kamu panggil.")
+                break
             else:
-                u_found = True  
-                p_found = False
+                account_found = True
+                print("Password salah!")
+                break
+        else :
+            account_found = False
 
-
-    if u_found == True and p_found == True :
-        print(f"Selamat datang, {username}!")
-        print("Masukkan command 'help' untuk daftar command yang dapat kamu panggil.")
-    elif u_found == True and p_found == False :
-        print("Password salah!")
-    elif u_found == False :
+    if account_found == False :
         print("Username tidak terdaftar")
+
+    
