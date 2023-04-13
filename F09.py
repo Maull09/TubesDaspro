d16e9c3f0a76540ef206d1f68277bf8d87f60370
Petani Kode <contoh@petanikode.com>
2023-04-13 11:01:25 +0700

    done

--- F09.py
+++ F09.py
@@ -2,65 +2,46 @@ import data
 import save
 
 def laporanjin() :
-    jumlah_pengumpul = save.hitung_jin("pengumpul")
-    jumlah_pembangun = save.hitung_jin("pembangun")
-    total = jumlah_pengumpul + jumlah_pembangun
+    jumlah_pengumpul = save.hitung_jin("pengumpul") # Memanggil Fungsi hitung_candi untuk menghitung jumlah jin khususnya jin pengumpul
+    jumlah_pembangun = save.hitung_jin("pembangun") # Memanggil Fungsi hitung_candi untuk menghitung jumlah jin khususnya jin pembangun
+    total_jin = jumlah_pengumpul + jumlah_pembangun # Menghitung jumlah seluruh jin
 
-    max_candi = 0
+    jumlah_candi = 0 # Total Jumlah Candi
+    max_candi = 0 
 
-    for i in range(1001) :
-        for j in range(1001) :
+    for i in range(101) :
+        for j in range(101) :
             if data.users[i][0] == data.candi[j][1] :
                 data.users[i][3] += 1
     
     max_candi = data.users[3][3]
     min_candi = data.users[3][3]
-    terajin = data.users[3][0]
-    termalas = data.users[3][0]
+    terajin = "null"
+    termalas = "null"
 
-    #cari candi terbanyak
-    for i in range(1001) :
-        if data.users[i][3] >= max_candi :
+    for i in range(101) :
+        if data.users[i][3] > max_candi :
             max_candi = data.users[i][3]
-
-    #mencari jin terajin dengan candi terbanyak sesuai leksikografis
-    for i in range(1001) :
-        if data.users[i][3] == max_candi :
-            if data.users[i][j] > terajin :
-                terajin = data.users[i][3]
-
-    #mencari candi tersedikit        
-    for i in range(1001) :
+            terajin = data.users[i][1] # Jin Terajin
+        
+    for i in range(101) :
         if data.users[i][3] < min_candi :
             min_candi = data.users[i][3]
+            termalas = data.users[i][1] # Jin Termalas 
+    
+    print("Total Jin :",total_jin)
+    print("Total Jin Pengumpul :", jumlah_pengumpul)
+    print("Total Jin Pembangun :", jumlah_pembangun)
+    print("Jin Terajin :", terajin)
+    print("Jin Termalas :", termalas)
+    print("Jumlah Pasir :", save.hitung_pasir( ))
+    print("Jumlah Air :", save.hitung_air())
+    print("Jumlah batu :", save.hitung_batu())
 
-    #mencari jin termalas dengan candi tersedikit sesuai leksikografis
-    for i in range(1001) :
-        if data.users[i][3] == min_candi :
-            if data.users[i][j] < terajin :
-                terajin = data.users[i][3]
+   
+    
 
-    pasir = 0
-    air = 0
-    batu = 0
-
-    for i in range(1001) :
-        if data.candi[i][2] == terajin :
-            for j in range(1001) :
-                pasir += data.candi[i][3] 
-            for j in range(1001) :
-                air += data.candi[i][4] 
-            for j in range(1001) :
-                batu += data.candi[i][5]
-                
+    #handling letak alfabetis
     #print
-    print(f"Total Jin : {total}")
-    print(f"Total Jin Pengumpul : {jumlah_pengumpul}")
-    print(f"Total Jin Pembangun : {jumlah_pembangun}")
-    print(f"Jin Terajin : {terajin}")
-    print(f"Jin Termalas : {termalas}")
-    print(f"Jumlah Pasir : {pasir}")
-    print(f"Jumlah Air : {air}")
-    print(f"Jumlah Batu : {batu}")
 
                 
