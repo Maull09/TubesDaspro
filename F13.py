df6654ced33b6f51d3bf3edbae4ef3da26c03a08
khaylabelva <belvakhayla@gmail.com>
2023-04-15 21:07:45 +0700

    F13

--- F13.py
+++ F13.py
@@ -13,9 +13,10 @@ def load() :
     # Validasi folder
     folder_name = args.folder_name
     if not os.path.exists(folder_name): # Jika folder tidak terdapat pada os path maka menghasilkan output file tidak ditemukan
-        print(f"Folder \"{folder_name}\" tidak ditemukan.")
+        print(f"Folder \"{folder_name}\" tidak ditemukan.") # Program keluar
     else: # Jika nama folder terdapat di os path maka file akan berhasil di load
         print("Loading...")
+        # Menjalankan prosedur load data
         reload(folder_name,"candi.csv", data.candi, 5)
         reload(folder_name,"bahan_bangunan.csv", data.bahan_bangunan, 3)
         reload(folder_name,"user.csv", data.users, 4)
